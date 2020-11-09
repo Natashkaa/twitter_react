ff47bda34c157fe75ec8130ae48d5d3e68b16cac
Enot <natasha.cat982404@gmail.com>
2020-11-09 21:33:48 +0200

    Working on main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
+import people from './icons/people.png';
+import search from './icons/search.png';
+import cloud from './icons/cloud.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid m-0 p-0">
+      <div className="firstRow row no-gutters">
+        <div className="birdSide">
+          <div className="text">
+            <ul className="simpleList">
+              <li><img className="icons" src={search}/> Читайте о том, что вам интересно.</li>
+              <li><img className="icons" src={people}/> Узнайте, о чем говорят в мире.</li>
+              <li><img className="icons" src={cloud}/> Присоединяйтесь к общению.</li>
+            </ul>
+          </div>
+          
+
+        </div>
+        <div className="darkSide">
+          darek
+        </div>
+      </div>
+      <div className="secondRow row no-gutters">
+        here will be a lot of links
+      </div>
     </div>
   );
 }
