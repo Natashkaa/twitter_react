9ef3bb54dde73a9fb3fd87c623ea524471e56876
Enot <natasha.cat982404@gmail.com>
2021-02-16 23:37:16 +0200

    Almost done with birth inputs in modal
    year input left

--- src/Components/selectDay.jsx
+++ src/Components/selectDay.jsx
@@ -1,36 +1,61 @@
 import React, { Component } from "react";
-import { createPortal } from "react-dom";
 import "../styles/modal.css"
 
-const LongMonths = [
-    {january: 31},
-    {march: 31},
-    {april: 30},
-    {may: 31},
-    {june: 30},
-    {july: 31},
-    {august: 31},
-    {september: 30},
-    {october: 31},
-    {november: 30},
-    {december: 31}
-]
+const LongMonths = {
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31
+}
+var daysList = [];
+
+function CompleteMonth(year, month){
+  daysList = [];
+  if(month == "february"){
+    if(parseInt(year) % 4){
+      for (let index = 1; index <= 28; index++) {
+        daysList.push(<option key={`key-${index}`} value={index}>{index}</option>)
+      }
+    }
+    else{
+      for (let index = 1; index <= 29; index++) {
+        daysList.push(<option key={`key-${index}`} value={index}>{index}</option>)
+      }
+    }
+  }
+  else{
+    for (let index = 1; index <= LongMonths[month]; index++) {
+      daysList.push(<option key={`key-${index}`} value={`${index}`}>{index}</option>)
+    }
+  }
+}
 
 export default class SelectDay extends Component {
-      render() {
-          if (this.props.selectedMonth != "february") {
-              return(
-              <div>
-                <select id="Birth_Month" className="input-birth-month" >
-                    <option value="december">Декабрь</option><option value="january">Январь</option><option value="february">Февраль</option>
-                    <option value="march">Март</option><option value="april">Апрель</option><option value="may">Май</option>
-                    <option value="june">Июнь</option><option value="july">Июль</option><option value="august">Август</option>
-                    <option value="september">Сентябрь</option><option value="october">Октябрь</option><option value="november">Ноябрь</option>
-                </select>
-            </div>,
-              document.getElementById("modal_root")
-            ) 
-          }
-          
-        }
+  constructor(props){
+    super(props);
+    this.state = { 
+      month: props.selectedMonth,
+      year: props.selectedYear
+    }
   }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.selectedMonth !== this.props.selectedMonth || nextProps.selectedYear !== this.props.selectedYear) {
+      this.setState({ month: nextProps.selectedMonth, year: nextProps.selectedYear});
+    }
+  }
+  render() {
+    CompleteMonth(this.state.year, this.state.month);
+    return(
+      <select id="Birth_Day" value={this.props.selectedDay} className="input-birth-month" onChange={this.props.onDayChange}>
+        {daysList}
+      </select>)
+  }
+}
--- src/Pages/main.jsx
+++ src/Pages/main.jsx
@@ -9,25 +9,55 @@ import bird from  '../icons/bird.png';
 import Modal from "./modal";
 import SelectDay from "../Components/selectDay";
 
+const LongMonths = {
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31
+}
+
 export default class App extends Component {
   constructor(props){
     super(props);
     this.handleMonthChange = this.handleMonthChange.bind(this);
+    this.handleDayChange = this.handleDayChange.bind(this);
+    this.handleYearChange = this.handleYearChange.bind(this);
     this.state = { 
       Full_Name: "",
       User_Login: "",
-      Birth_Month: "march",
-      Birth_Day: "",
-      Birth_Year: "",
+      Birth_Month: "january",
+      Birth_Day: "1",
+      Birth_Year: "1901",
       User_Password: ""
     }
   }
  
   handleMonthChange = event => {
-    console.log('old month '+ this.state.Birth_Month);
     this.setState({ Birth_Month: event.target.value});
+    if(LongMonths[event.target.value] < parseInt(this.state.Birth_Day)){
+      this.setState({ Birth_Day: 1});
+    }
+  }
+  handleDayChange = event => {
+    this.setState({ Birth_Day: event.target.value});
+  }
+  handleYearChange = event => {
+    this.setState({ Birth_Year: event.target.value});
+    console.log(parseInt(event.target.value) % 4);
+    if(!parseInt(event.target.value) % 4 && this.state.Birth_Month == " february"){
+      this.setState({ Birth_Day: 1});
+    }
   }
   render() {
+    console.log('current date ' + this.state.Birth_Month + " " + this.state.Birth_Day + " " + this.state.Birth_Year);
     return (
       <div className="App container-fluid m-0 p-0">
         <div className="first-row row flex-row no-gutters">
@@ -84,21 +114,43 @@ export default class App extends Component {
                                   <div className="modal-text3">Эта информация не будет общедоступной. Подтвердите свой возраст, даже если эта учетная запись предназначена для компании, домашнего животного и т. д.</div>
                                   
                                   <div className="birth-container">
-                                  <div className="user-birth-month">
-                                    <div className="shell">
-                                      <div className="shell-row1">
-                                        <div className="div-name">Месяц</div>
+                                    <div className="user-birth-month">
+                                      <div className="shell">
+                                        <div className="shell-row1">
+                                          <div className="div-name">Месяц</div>
+                                        </div>
+                                        <select id="Birth_Month" className="input-birth-month" value={this.state.Birth_Month} onChange={this.handleMonthChange}>
+                                          <option value="december">Декабрь</option><option value="january">Январь</option><option value="february">Февраль</option>
+                                          <option value="march">Март</option><option value="april">Апрель</option><option value="may">Май</option>
+                                          <option value="june">Июнь</option><option value="july">Июль</option><option value="august">Август</option>
+                                          <option value="september">Сентябрь</option><option value="october">Октябрь</option><option value="november">Ноябрь</option>
+                                        </select>
                                       </div>
-                                      <select id="Birth_Month" className="input-birth-month" value={this.state.Birth_Month} onChange={this.handleMonthChange}>
-                                        <option value="december">Декабрь</option><option value="january">Январь</option><option value="february">Февраль</option>
-                                        <option value="march">Март</option><option value="april">Апрель</option><option value="may">Май</option>
-                                        <option value="june">Июнь</option><option value="july">Июль</option><option value="august">Август</option>
-                                        <option value="september">Сентябрь</option><option value="october">Октябрь</option><option value="november">Ноябрь</option>
-                                      </select>
-                                      {/* <SelectDay selectedMonth={this.state.Birth_Month}/> */}
                                     </div>
+
+                                    <div className="user-birth-day">
+                                      <div className="shell">
+                                        <div className="shell-row1">
+                                          <div className="div-name">Число</div>
+                                        </div>
+                                        <SelectDay onDayChange={this.handleDayChange} selectedMonth={this.state.Birth_Month} selectedDay={this.state.Birth_Day} selectedYear={this.state.Birth_Year}/>
+                                      </div>
+                                    </div>
+
+                                    <div className="user-birth-year">
+                                      <div className="shell">
+                                        <div className="shell-row1">
+                                          <div className="div-name">Год</div>
+                                        </div>
+                                        <select id="Birth_Year" className="input-birth-year" value={this.state.Birth_Year} onChange={this.handleYearChange}>
+                                          <option value="1901">1901</option><option value="1902">1902</option><option value="1903">1903</option>
+                                          <option value="1904">1904</option>
+                                        </select>
+                                      </div>
+                                    </div>
+
                                   </div>
-                                  </div>
+                                  
                                 </div>
                                 
                                 
