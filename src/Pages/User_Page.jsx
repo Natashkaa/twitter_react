562f6998d9078ab048de2425f6c6667d008b7689
Enot <natasha.cat982404@gmail.com>
2021-03-22 23:14:47 +0200

    Creating user profile block

--- src/Pages/User_Page.jsx
+++ src/Pages/User_Page.jsx
@@ -10,6 +10,7 @@ import Profile from "../icons/profile_ic.png";
 import New_Tweet from "../icons/tweet_ic.png";
 import Test_Icon from "../icons/ana.jpg";
 import LogOut from "../icons/log_out_ic.png";
+import TestBcg from "../images/test_background.jpg";
 
 export default class User_Page extends Component{
     constructor(props){
@@ -47,67 +48,20 @@ export default class User_Page extends Component{
                 </div>
                 <div className="right-block">
                     <div className="top-fix-header">
-                        <button className="btn btn-warning">+TESETSDfsdf</button>
+                        <button className="btn-go-top">USER NAME</button>
+                        <div className="tweet-amount">1 000 твитов</div>
                     </div>
                     <div className="right-block-body">
-                    <button className="btn btn-warning">+TESETSDfsdf</button>
-                        <ol className="menu-list">
-                            <li><a href="#"><img className="special-icon-bird" src={bird}/></a></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Home}/><a href="https://www.instagram.com/" target="_blank" className="li-name">Главная</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Search}/><a href="#" className="li-name">Поиск</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Notif}/><a href="#" className="li-name">Уведомления</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Message}/><a href="#" className="li-name">Сообщения</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Profile}/><a href="#" className="li-name">Профиль</a></div></li>
-                            <li><button className="new-tweet-btn"><img className="menu-icons new-tweet-icon" src={New_Tweet}/>Твитнуть</button></li>
-                            <li className="logout-li">
-                                <div className="lil-image-container"><img className="lil-prof-icon" src={Test_Icon} alt="profile icon"/></div>
-                                <div className="lil-info-container">
-                                    <div className="username-info">USER NAME</div>
-                                    <div className="userlogin-info">@kek_lalala</div>
-                                </div>
-                                <div className="lil-image-container">
-                                    <button className="btn-logout" title="log out"><img className="menu-icons" src={LogOut} alt="profile icon"/></button>
-                                    </div>
-                            </li>
-                        </ol>
-                        <ol className="menu-list">
-                            <li><a href="#"><img className="special-icon-bird" src={bird}/></a></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Home}/><a href="https://www.instagram.com/" target="_blank" className="li-name">Главная</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Search}/><a href="#" className="li-name">Поиск</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Notif}/><a href="#" className="li-name">Уведомления</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Message}/><a href="#" className="li-name">Сообщения</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Profile}/><a href="#" className="li-name">Профиль</a></div></li>
-                            <li><button className="new-tweet-btn"><img className="menu-icons new-tweet-icon" src={New_Tweet}/>Твитнуть</button></li>
-                            <li className="logout-li">
-                                <div className="lil-image-container"><img className="lil-prof-icon" src={Test_Icon} alt="profile icon"/></div>
-                                <div className="lil-info-container">
-                                    <div className="username-info">USER NAME</div>
-                                    <div className="userlogin-info">@kek_lalala</div>
-                                </div>
-                                <div className="lil-image-container">
-                                    <button className="btn-logout" title="log out"><img className="menu-icons" src={LogOut} alt="profile icon"/></button>
-                                    </div>
-                            </li>
-                        </ol>
-                        <ol className="menu-list">
-                            <li><a href="#"><img className="special-icon-bird" src={bird}/></a></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Home}/><a href="https://www.instagram.com/" target="_blank" className="li-name">Главная</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Search}/><a href="#" className="li-name">Поиск</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Notif}/><a href="#" className="li-name">Уведомления</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Message}/><a href="#" className="li-name">Сообщения</a></div></li>
-                            <li><div className="lis-content"><img className="menu-icons" src={Profile}/><a href="#" className="li-name">Профиль</a></div></li>
-                            <li><button className="new-tweet-btn"><img className="menu-icons new-tweet-icon" src={New_Tweet}/>Твитнуть</button></li>
-                            <li className="logout-li">
-                                <div className="lil-image-container"><img className="lil-prof-icon" src={Test_Icon} alt="profile icon"/></div>
-                                <div className="lil-info-container">
-                                    <div className="username-info">USER NAME</div>
-                                    <div className="userlogin-info">@kek_lalala</div>
-                                </div>
-                                <div className="lil-image-container">
-                                    <button className="btn-logout" title="log out"><img className="menu-icons" src={LogOut} alt="profile icon"/></button>
-                                    </div>
-                            </li>
-                        </ol>
+                        <div className="user-background-block">
+                            <img className="user-background" src={TestBcg} title="your background"/>
+                        </div>
+                        <div className="user-data-block container-fluid m-0 p-0">
+                            <div className="row flex-row no-gutters first-row">
+                                <img className="user-prof-icon" src={Test_Icon}/>
+                                <input className="btn-edit-profile" type="button" value="Изменить профиль"></input>
+                            </div>
+                            <div>SDFSDFSDFSDFSDFSD</div>
+                        </div>
                     </div>
                 </div>
             </div>
